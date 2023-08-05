fc1e0c542e22815c8f7529f489c338d6b13fdb21
varell <ryukaa57@gmail.com>
2023-08-05 12:06:32 +0700

    first commit

---
+++ coba2/tugas.py
@@ -0,0 +1,20 @@
+
+bil1=int(input('Masukkan bilangan 1='))
+bil2=int(input('Masukkan bilangan 2='))
+
+def summation(a,b):
+    print("Hasil penjumlahan=",a+b)
+
+def subtraction(a,b):
+    print("Hasil pengurangan=",a-b)
+
+def multiplication(a,b):
+    print ("Hasil perkalian=",a*b)
+
+def distribution(a,b):
+    print ("Hasil pembagian=",a/b)
+
+summation(bil1,bil2)
+subtraction(bil1,bil2)
+multiplication(bil1,bil2)
+distribution(bil1,bil2)
